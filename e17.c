56166a83f12db575fddb72d5d7b569cd74bdaffb
Marks Ruziboev <112407232+marksruziboev@users.noreply.github.com>
2023-01-22 17:28:05 +0100

    int to char
    This program finds the number of letters that would be used if all the numbers from 1 to 1000 (one thousand) inclusive were written out in words.

--- e17.c
+++ e17.c
@@ -3,68 +3,94 @@
 #include <string.h>
 #include <time.h>
 
-char *strjoin(char *u, char *v)
+int lugat(int n)
 {
-    char *s;
+    if (n == 0)
+        return(0);
+    else if (n == 1)
+        return(3);//;("one");
+    else if (n == 2)
+        return(3);//("two");
+    else if (n == 3)
+        return (5);//("three");
+    else if (n == 4)
+        return (4);// ("four");
+    else if (n == 5)
+        return (4);//("five");
+    else if (n == 6)
+        return (3);//("six");
+    else if (n == 7)
+        return (5);//("seven");
+    else if (n == 8)
+        return (5);//("eight");
+    else if (n == 9)
+        return (4);//("nine");
+    else if (n == 10)
+        return (3);//("ten");
+    else if (n == 11)
+        return (6);//("eleven");
+    else if (n == 12)
+        return (6);//("twelve");
+    else if (n == 13)
+        return (8);//("thirteen");
+    else if (n == 14)
+        return (8);//("fourteen");
+    else if (n == 15)
+        return (7);//("fifteen");
+    else if (n == 16)
+        return (7);// ("sixteen");
+    else if (n == 17)
+        return (9);// ("seventeen");
+    else if (n == 18)
+        return(8);// ("eighteen");
+    else if (n == 19)
+        return(8);// ("nineteen");
+    else if (n == 20)
+        return(6);// ("twenty");
+    else if (n == 30)
+        return(6);// ("thirty");
+    else if (n == 40)
+          return(5); //("forty");
+    else if (n == 50)
+        return(5);// ("fifty");
+    else if (n == 60)
+        return(5);// ("sixty");
+    else if (n == 70)
+        return(7);// ("seventy");
+    else if (n == 80)
+        return(6);// ("eighty");
+    else if (n == 90)
+        return(6);// ("ninety");
+    else if (n == 100)
+        return(7); //("hundert");
+    else if (n == 1000)
+        return(11);// ("onethousand");
 
-    s = malloc(strlen(u)+ strlen(v)+1);
-    s[strlen(u)+ strlen(v)]= '\0';
-    while(*u)
-    {
-        *s = *u;
-        s++;
-        u++;
-    }
-    while(*v)
-    {
-        *s = *v;
-        v++;
-        s++;
-    }
-    return(s);
 }
-char *str(int n)
+int lenword(int n)
 {
-    char *s;
-    int a;
-
-    s = malloc(20);
-    s[0] = '0';
-    s[19] = '\0';
-    if (n == 1)
-        return("one");
-    else if ( n == 2)
-        return("two");
-    else if ( n == 3)
-        return("three");
-    else if ( n == 4)
-        return("four");
-    else if ( n == 5)
-        return("five");
-    else if ( n == 6)
-        return("six");
-    else if ( n == 7)
-        return("seven");
-    else if ( n == 8)
-        return("eight");
-    else if ( n == 9)
-        return("nine");
-    else if ( n == 10)
-        return("ten");
-    else if ( n == 20)
-        return("twenty");
-    else if ( n == 30)
-        return("thirty");
-    else if ( n /10 >= 4 && n/10 <= 9)
-        return(strjoin(str(n/10), "ty"));
-    else if ( n/100 >= 1 && n/100 <= 9)
-        return(strjoin(str(n/100), "hundert"));
-    else if ( n == 1000)
-        return ("onethousand");
+    if ((n %10 == 0 && n < 100)|| n == 1000)
+        return(lugat(n));
+    else if (n >= 100 && n < 1000 && n % 100 == 0)
+        return(lugat(n/100)+ lugat(100));
+    else if (n >= 1 && n < 20)
+        return(lugat(n));
+    else if (n > 20 && n < 100)
+        return(lugat(n - n%10)+ lugat(n%10));
+    else if (n > 100 && n < 1000 && n%100 <= 20)
+        return(lugat(n/100) + lugat(100) + 3 + lugat(n%100));
+    else if (n > 100 && n < 1000 && n%100 > 20)
+        return(lugat(n/100) + lugat(100) + 3 + lugat(10*((n%100)/10))+ lugat((n%100)%10));
 }
 int main()
 {
-    int i = 600;
-    printf("%s",str(i));
+    int n = 1, sum = 0;
+    while (n <= 1000)
+    {
+        sum = sum + lenword(n);
+        n++;
+    }
+    printf("%d", sum);
+
     return(0);
 }
